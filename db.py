929e29f9eef8ff14f425b349819d2883fa99c58b
gunaydogukan <21010310032@ogrenci.bartin.edu.tr>
2023-12-21 14:53:19 +0300

    first commit

---
+++ db.py
@@ -0,0 +1,301 @@
+from datetime import datetime
+from flask import Flask
+import mysql.connector
+from werkzeug.security import generate_password_hash
+from user import User
+import mysql.connector
+from contextlib import closing
+
+mydb = mysql.connector.connect(
+  host="localhost",
+   user="root",
+  password="dogukan1903"
+)
+
+mycursor = mydb.cursor()
+mycursor.execute("USE ChatDB")
+
+""""""
+mycursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL
+    )
+""")
+
+mycursor.execute("""
+    CREATE TABLE IF NOT EXISTS rooms (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        created_by VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+""")
+
+mycursor.execute("""
+    CREATE TABLE IF NOT EXISTS room_members (
+    room_id INT,
+    username VARCHAR(255) NOT NULL,
+    room_name VARCHAR(255) NOT NULL,
+    added_by VARCHAR(255) NOT NULL,
+    added_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    is_room_admin BOOLEAN,
+    PRIMARY KEY (room_id, username),  
+    FOREIGN KEY (room_id) REFERENCES rooms(id)    
+    )
+""")
+
+
+
+def save_user(username, email, password):
+    
+    try:
+        # Şifreyi güvenli bir şekilde hashle
+        pasword_hash = generate_password_hash(password)
+
+        # Kullanıcıyı veritabanına ekle
+        mycursor.execute("""
+            INSERT INTO users (username, email, password)
+            VALUES (%s, %s, %s)
+        """, (username, email, pasword_hash))
+
+        mydb.commit()
+    except mysql.connector.IntegrityError as e:
+        if e.errno==1062:
+             raise ValueError("Bu kullanıcı adı veya e-posta adresi zaten kullanılıyor.")
+        else:
+            raise e
+    
+
+def get_user(username):
+    user = None
+    try:
+        with closing(mydb.cursor()) as mycursor:
+            mycursor.execute("""
+                SELECT id, username, email, password FROM users
+                WHERE username = %s
+            """, (username,))
+            
+            user_data = mycursor.fetchone()
+            print("USERDATA FETCHONE")
+            
+            if user_data:
+                print("USERDATA İFİN İÇİ = ",user_data)
+                
+                user_id, username, email, password_hash = user_data
+                
+                print("VERİLER = ",user_id, username, email, password_hash)
+                
+                user = User(user_id, email, password_hash)
+                
+                mycursor.clear_attributes()
+                print("USERE GİRDİ =",user)
+                return User(user_id, email, password_hash)
+    except Exception as e:
+        print(f"HATA ? ? ******** =  {e}")
+    return None
+
+
+def save_room(room_name,created_by):
+    
+    room_id = None
+    
+    try:
+        # Kullanıcı kontrolü
+        user = get_user(created_by)
+        if not user:
+            raise ValueError("Kullanıcı bulunamadı veya geçersiz.")
+
+        # Odayı veritabanına ekle
+        created_at = datetime.now()
+        mycursor.execute("""
+            INSERT INTO rooms (name, created_by, created_at)
+            VALUES (%s, %s, %s)
+        """, (room_name, created_by, created_at))
+        
+        mycursor.execute("SELECT LAST_INSERT_ID()") 
+        room_id = mycursor.fetchone()[0]
+
+        mydb.commit()
+    except Exception as e:
+        print(f"HATA ? ? ******** = {e}")
+    
+    add_room_member(room_id, room_name, created_by, created_by, is_room_admin=True)
+    return room_id
+        
+
+def update_room(room_id, room_name):
+    try:
+        # Odanın adını güncelle
+        mycursor.execute("""
+            UPDATE rooms
+            SET name = %s
+            WHERE id = %s
+        """, (room_name, room_id))
+
+        # Odaya ait üyelerin odanın adını güncelle
+        mycursor.execute("""
+            UPDATE room_members
+            SET room_name = %s
+            WHERE room_id = %s
+        """, (room_name, room_id))
+
+        mydb.commit()
+        print(f"Room {room_id} updated successfully.")
+    except mysql.connector.Error as e:
+        print(f"Hata: {e}")
+
+def get_room(room_id):
+    try:
+        mycursor.execute("""
+            SELECT * FROM rooms
+            WHERE id = %s
+        """, (room_id,))
+
+        room_data = mycursor.fetchone()
+
+        if room_data:
+            room_id, name, created_by, created_at = room_data
+            print(f"Room found: id={room_id}, name={name}, created_by={created_by}, created_at={created_at}")
+            return room_data
+        else:
+            print("Room not found.")
+            return None
+    except mysql.connector.Error as e:
+        print(f"Hata: {e}")
+        return None
+
+def add_room_member(room_id, room_name, username, added_by, is_room_admin=False):
+    try:
+        # Oda üyesini eklemek için INSERT sorgusu
+        added_at = datetime.now()
+        mycursor.execute("""
+            INSERT INTO room_members (room_id, username, room_name, added_by, added_at, is_room_admin)
+            VALUES (%s, %s, %s, %s, %s, %s)
+        """, (room_id, username, room_name, added_by, added_at, is_room_admin))
+
+        # Veritabanındaki değişiklikleri kaydetme
+        mydb.commit()
+
+        print("Oda üyesi başarıyla eklendi.")
+    except mysql.connector.Error as e:
+        print(f"Hata: {e}")
+
+def add_room_members(room_id, room_name, usernames, added_by):
+    try:
+        # Oda üyelerini eklemek için INSERT sorgusu
+        added_at = datetime.now()
+        is_room_admin = False
+
+        insert_query = """
+            INSERT INTO room_members (room_id, username, room_name, added_by, added_at, is_room_admin)
+            VALUES (%s, %s, %s, %s, %s, %s)
+        """
+
+        # Her kullanıcı için INSERT işlemi gerçekleştir
+        for username in usernames:
+            mycursor.execute(insert_query, (room_id, username, room_name, added_by, added_at, is_room_admin))
+
+        # Veritabanındaki değişiklikleri kaydetme
+        mydb.commit()
+
+        print("Oda üyeleri başarıyla eklendi.")
+    except mysql.connector.Error as e:
+        print(f"Hata: {e}")
+
+def remove_room_members(room_id, usernames):
+    try:
+        # Odaya ait belirli kullanıcıları sil
+        mycursor.execute("""
+            DELETE FROM room_members
+            WHERE room_id = %s AND username IN %s
+        """, (room_id, tuple(usernames)))
+
+        mydb.commit()
+        print(f"Room members removed successfully.")
+    except mysql.connector.Error as e:
+        print(f"Hata: {e}")
+
+def get_room_members(room_id):
+    try:
+        mycursor.execute("""
+            SELECT * FROM room_members
+            WHERE room_id = %s
+        """, (room_id,))
+
+        room_members_data = mycursor.fetchall()
+
+        if room_members_data:
+            print(f"Room members found for room_id={room_id}")
+            return room_members_data
+        else:
+            print("No room members found.")
+            return []
+    except mysql.connector.Error as e:
+        print(f"Hata: {e}")
+        return []
+
+def get_rooms_for_user(username):
+    try:
+        mycursor.execute("""
+            SELECT * FROM room_members
+            WHERE username = %s
+        """, (username,))
+
+        user_rooms_data = mycursor.fetchall()
+
+        if user_rooms_data:
+            print(f"Rooms found for user {username}")
+            return user_rooms_data
+        else:
+            print(f"No rooms found for user {username}")
+            return []
+    except mysql.connector.Error as e:
+        print(f"Hata: {e}")
+        return []
+
+def is_room_member(room_id, username):
+    try:
+        mycursor.execute("""
+            SELECT COUNT(*) FROM room_members
+            WHERE room_id = %s AND username = %s
+        """, (room_id, username))
+
+        count = mycursor.fetchone()[0]
+
+        if count > 0:
+            print(f"{username} is a member of room_id={room_id}")
+            return True
+        else:
+            print(f"{username} is not a member of room_id={room_id}")
+            return False
+    except mysql.connector.Error as e:
+        print(f"Hata: {e}")
+        return False
+
+def is_room_admin(room_id, username):
+    try:
+        mycursor.execute("""
+            SELECT COUNT(*) FROM room_members
+            WHERE room_id = %s AND username = %s AND is_room_admin = 1
+        """, (room_id, username))
+
+        count = mycursor.fetchone()[0]
+
+        if count > 0:
+            print(f"{username} is an admin of room_id={room_id}")
+            return True
+        else:
+            print(f"{username} is not an admin of room_id={room_id}")
+            return False
+    except mysql.connector.Error as e:
+        print(f"Hata: {e}")
+        return False
+
+
+
+
+
+
---
+++ server.py
@@ -0,0 +1,136 @@
+from flask import Flask, render_template, redirect, url_for,request,redirect,url_for,session
+from flask_socketio import SocketIO,send,join_room,leave_room,send,emit
+from flask_login import LoginManager,login_required, login_user, logout_user,current_user
+
+from db import add_room_members, get_room, get_room_members, get_rooms_for_user, get_user, is_room_member, save_room, save_user
+from user import User
+from flask_session import Session
+
+
+app=Flask(__name__)
+app.secret_key = "my secret key"
+app.config['SECRET']="123456"
+socketio=SocketIO(app, cors_allowed_origins="*") 
+login_manager = LoginManager()
+login_manager.login_view='login'
+login_manager.init_app(app)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+
+
+@app.route('/')
+def home():
+    if not session.get("username"):
+        return redirect("/login")
+    else:
+        rooms = get_rooms_for_user(session.get("username"))    
+    return render_template("index.html", rooms=rooms)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    
+    if session.get("username"):
+        print("çalışıyor")
+        return render_template('index.html')
+    
+    message = ''
+    if request.method == 'POST':
+        session['username']=request.form.get('username')
+        session['password_input']=request.form.get('password')
+        user = get_user(session['username'])
+                
+        if user and user.check_password( session['password_input']):
+            login_user(user)
+            return redirect(url_for('home'))
+        else:
+            message='Giriş başarısız'
+    
+    return render_template('login.html',message=message)
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    
+    if session.get("username"):
+        print("çalışıyor")
+        return render_template('index.html')
+
+    message = ''
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        save_user(username, email, password)
+        return redirect(url_for('login'))
+    
+    return render_template('signup.html', message=message)
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/create_room', methods=['GET', 'POST'])
+def create_room():
+    
+    if not session.get("username"):
+        return redirect("/login")  
+    
+    message = ""
+    
+    if request.method == 'POST':
+        room_name = request.form.get('room_name')
+        usernames = [username.strip() for username in request.form.get('members').split(',')]
+
+        # Oda oluşturma işlemleri burada yapılır
+        if len(room_name) and len(usernames):
+            room_id = save_room(room_name,  session.get("username"))
+            if session.get("username") in usernames:
+                usernames.remove(session.get("username"))
+            add_room_members(room_id, room_name, usernames, session.get("username"))
+            print("view room gidiiş")
+            return redirect(url_for('view_room', room_id=room_id))
+        else:
+            message = "Oda oluşturulamadı."
+    return render_template('create_room.html', message=message)
+
+
+@app.route('/rooms/<room_id>/')
+def view_room(room_id):
+    
+    if not session.get("username"):
+        return redirect("/login") 
+    
+    print("view room")
+    room=get_room(room_id)
+    if room and is_room_member(room_id,session['username']):
+        room_uye=get_room_members(room_id)
+        print("ifin içine girdik ","room üye = ",room_uye)
+        return render_template('view_room.html',username =session.get("username") ,room=room,room_members=room_uye)
+    else:
+        return "Oda bulunamadı",404
+
+
+@socketio.on('send_message')
+def handle_send_message_event(data):
+    app.logger.info("{},{} numaralı odaya mesah gönderdi: {}".format(data['username'],data['room'],data['message']))
+    socketio.emit('receive_message', data, room=data['room'])
+
+@socketio.on('join_room')
+def handle_join_room_event(data):
+    app.logger.info("{} adlı kullanıcı {} odasına katıldı".format(data['username'],data['room']))
+    join_room(data['room'])
+    socketio.emit('join_room_announcement',data)
+
+@login_manager.user_loader
+def load_user(username):
+    return get_user(username)
+
+if __name__ == '__main__':
+    socketio.run(app,debug=True,)
